8aa8e858b4d0987f4a370925060f3ae6d4b5736b
automatedbymegan <automatedbymegan@gmail.com>
2025-03-26 16:27:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function updateDateTime() {
   const dateEl = document.getElementById('date');
   const timeEl = document.getElementById('time');
 
-  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
   const dateStr = now.toLocaleDateString(undefined, options);
   let hours = now.getHours();
   let minutes = now.getMinutes().toString().padStart(2, '0');
